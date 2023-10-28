ba27eeb8f61e1e5d6e852e30c16275a182104fdb
Jesus <jesus.fdz93@gmail.com>
2023-10-28 11:54:54 -0500

    iniciando

---
+++ js/index.js
@@ -0,0 +1,35 @@
+// OBTENER ELEMENTOS DEL DOM
+// const h1TituloPrincipal = document.getElementById("h1TituloPrincipal");
+// console.log(h1TituloPrincipal);
+
+// const texto_blanco = document.getElementsByClassName("texto-blanco");
+// console.log(texto_blanco);
+
+// const h1s = document.getElementsByTagName("input");
+// console.log(h1s);
+
+// const h1TituloPrincipal = document.querySelector("#h1TituloPrincipal");
+// console.log(h1TituloPrincipal);
+
+// const inputs = document.querySelectorAll("input");
+// console.log(inputs);
+
+// CREAR ELEMENTOS EN EL DOM
+// const seccion_secundaria = document.getElementById("seccion_secundaria");
+// const parrafo = document.createElement("p");
+// const texto = document.createTextNode("hola mundo");
+
+// parrafo.appendChild(texto);
+// seccion_secundaria.appendChild(parrafo);
+
+// EVENTOS DEL DOM
+const saludar = () => {
+  console.log("Hola mundo");
+};
+
+const despedir = () => {
+  console.log("Adios");
+};
+
+const btnDespedir = document.getElementById("btnDespedir");
+btnDespedir.addEventListener("click", despedir);
